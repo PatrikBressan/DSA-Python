53bf8bc38d8efa4018e8751d6d7c0999244f88cc
PatrikBressan <patrikbressan@gmail.com>
2023-09-29 19:45:10 -0400

    Prova 1

---
+++ Cap7/prova_1.py
@@ -0,0 +1,28 @@
+'''
+Prova 1: Patrik Bressan
+Arquivo de testes práticos.
+'''
+
+teste_2 = 5
+print(teste_2)
+
+result = [x for x in range(10)]
+print(result)
+
+s = 'Data Science Academy'
+#s(2) = 'd'
+print(s)
+print(s[2])
+
+result2 = [x**2 for x in range(1,11)]
+print(result2)
+
+nome = 'Patrik'
+sobrenome = 'Bressan'
+nome_completo = nome + sobrenome
+print(nome_completo)
+
+
+dic3 = {}
+dic3.append(2)
+print(dic3)
