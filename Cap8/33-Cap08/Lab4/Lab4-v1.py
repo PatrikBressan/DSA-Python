ca9f980fafdf0795f9b80bf740cc2a35df0005bf
PatrikBressan <patrikbressan@gmail.com>
2023-10-19 17:05:22 -0400

    Iniciando jogo Hangman com POO em Python.

---
+++ Cap8/33-Cap08/Lab4/Lab4-v1.py
@@ -0,0 +1,86 @@
+# Hangman Game (Jogo da Forca) 
+# Programação Orientada a Objetos
+
+# Import
+import random
+
+# Board (tabuleiro)
+board = ['''
+
+>>>>>>>>>>Hangman<<<<<<<<<<
+
++---+
+|   |
+    |
+    |
+    |
+    |
+=========''', '''
+
++---+
+|   |
+O   |
+    |
+    |
+    |
+=========''', '''
+
++---+
+|   |
+O   |
+|   |
+    |
+    |
+=========''', '''
+
+ +---+
+ |   |
+ O   |
+/|   |
+     |
+     |
+=========''', '''
+
+ +---+
+ |   |
+ O   |
+/|\  |
+     |
+     |
+=========''', '''
+
+ +---+
+ |   |
+ O   |
+/|\  |
+/    |
+     |
+=========''', '''
+
+ +---+
+ |   |
+ O   |
+/|\  |
+/ \  |
+     |
+=========''']
+
+
+# Classe
+class Hangman:
+	# Método Construtor
+     def __init__(self):
+          self.tituloGame = 'Jogo da Forca - Hangman'
+          print("Construtor chamado para criar um objeto desta classe!")
+
+	# Método para adivinhar a letra
+     
+	
+	# Método para verificar se o jogo terminou
+		
+	# Método para verificar se o jogador venceu
+		
+	# Método para não mostrar a letra no board
+		
+	# Método para checar o status do game e imprimir o board na tela
+
