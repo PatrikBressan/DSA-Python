1a12ee5cfd8261049a602dd31b2373489fc2880b
PatrikBressan <patrikbressan@gmail.com>
2024-03-15 14:57:19 -0400

    Cap13 concluído.

---
+++ Cap13/teste.py
@@ -0,0 +1,14 @@
+lista1 = [1,2,3]
+tupla1 = (10,20,30)
+dict1 = {'primeiro': 1, 'segundo':  2}
+
+print([x for x in range(10) if x < 5])
+
+dict3 = {}
+print(dict3)
+
+print([x for x in range(10)])
+
+s = 'Data Science Academy'
+print(s[:4])
+
